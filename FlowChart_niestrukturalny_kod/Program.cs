13dd57c4450c333735bbaa89725d2112ae0634a2
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-03 10:26:12 +0100

    #01-wysłanie szkieletu

--- FlowChart_niestrukturalny_kod/Program.cs
+++ FlowChart_niestrukturalny_kod/Program.cs
@@ -1,2 +1,48 @@
-﻿g// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+string input = Console.ReadLine();
+int[] data = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
+int x = data[0];
+int y = data[1];
+int z = data[2];
+found:
+    if( x > 0)
+    {
+        if (y > 0)
+        {
+            x = x - 1;
+            y = y - 1;
+            Console.Write("C");
+            goto found;
+        }
+        else
+        {
+            Console.Write("D");
+            if(z > 0)
+            {
+        punkttest:
+            Console.WriteLine("");
+            goto exit;
+        }
+        else
+        {
+
+            goto IfNo;
+        }
+        }
+}
+else
+{
+
+    Console.Write("E");
+    IfNo:
+    Console.Write("G");
+    goto punkttest;
+
+}
+exit:
+#if DEBUG
+    Console.WriteLine("Press enter to close...");
+    Console.ReadLine();
+#endif
+ 

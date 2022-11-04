6a4b02a1091b8ee9ebefc3af8b711d9d045cba3a
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-04 13:03:45 +0100

    Poprawka by dzialalo na moodlu

--- FlowChart_niestrukturalny_kod/Program.cs
+++ FlowChart_niestrukturalny_kod/Program.cs
@@ -1,133 +1,51 @@
-﻿using System.Data.Common;
+﻿using System;
 using System.Diagnostics.CodeAnalysis;
-string input = Console.ReadLine();
-int[] data = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
-int x = data[0];
-int y = data[1];
-int z = data[2];
-begining:
-if (x > 0)
+
+namespace FlowChart
 {
-    if (y > 0)
+    class Program
     {
-        x = x - 1;
-        y = y - 1;
-        Console.Write("C");
-        goto begining;
-    }
-    else
-    {
-        Console.Write("D");
-        
-    }
-
-}
-else
-{
-    Console.Write("E");
-    goto g;
-}
-if(z > 0)
-{
-    Console.WriteLine();
-    goto end;
-}
-
-g:
-Console.Write("G");
-Console.WriteLine();
-
-end:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*int x = data[0];
-int y = data[1];
-int z = data[2];
-found:
-
-if (x > 0)
-{
-    if (y > 0)
-    {
-        x = x - 1;
-        y = y - 1;
-        Console.Write("C");
-        goto found;
-    }
-    else
-    {
-        Console.Write("D");
-        if (z > 0)
+        static void Main()
         {
-        punkttest:
-            Console.WriteLine("");
-            goto exit;
-        }
-        else
-        {
+            string input = Console.ReadLine();
+            int[] data = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
+            int x = data[0];
+            int y = data[1];
+            int z = data[2];
+        begining:
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    x = x - 1;
+                    y = y - 1;
+                    Console.Write("C");
+                    goto begining;
+                }
+                else
+                {
+                    Console.Write("D");
 
-            goto IfNo;
+                }
+
+            }
+            else
+            {
+                Console.Write("E");
+                goto g;
+            }
+            if (z > 0)
+            {
+                goto end;
+            }
+
+        g:
+            Console.Write("G");
+
+
+        end:
+            Console.WriteLine();
+
         }
     }
 }
-else
-{
-    goto punkttest;
-    Console.Write("E");
-    IfNo:
-    Console.Write("G");
-    
-
-}
-exit:*/
-#if DEBUG
-    Console.WriteLine("Press enter to close...");
-    Console.ReadLine();
-#endif
- 

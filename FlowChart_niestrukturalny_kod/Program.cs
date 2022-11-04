b4e19dc0cd0dbb347ffc08d104eaf98d68184edf
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-04 12:52:15 +0100

    przygotowana baza

--- FlowChart_niestrukturalny_kod/Program.cs
+++ FlowChart_niestrukturalny_kod/Program.cs
@@ -5,21 +5,106 @@ int[] data = Array.ConvertAll<string, int>(input.Split(" "), int.Parse);
 int x = data[0];
 int y = data[1];
 int z = data[2];
-found:
-    if( x > 0)
+begining:
+if (x > 0)
+{
+    if (y > 0)
     {
-        if (y > 0)
+        x = x - 1;
+        y = y - 1;
+        Console.Write("C");
+        goto begining;
+    }
+    else
+    {
+        Console.Write("D");
+        
+    }
+
+}
+else
+{
+    Console.Write("E");
+    goto g;
+}
+if(z > 0)
+{
+    Console.WriteLine();
+    goto end;
+}
+
+g:
+Console.Write("G");
+Console.WriteLine();
+
+end:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*int x = data[0];
+int y = data[1];
+int z = data[2];
+found:
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        x = x - 1;
+        y = y - 1;
+        Console.Write("C");
+        goto found;
+    }
+    else
+    {
+        Console.Write("D");
+        if (z > 0)
         {
-            x = x - 1;
-            y = y - 1;
-            Console.Write("C");
-            goto found;
-        }
-        else
-        {
-            Console.Write("D");
-            if(z > 0)
-            {
         punkttest:
             Console.WriteLine("");
             goto exit;
@@ -29,18 +114,18 @@ found:
 
             goto IfNo;
         }
-        }
+    }
 }
 else
 {
-
+    goto punkttest;
     Console.Write("E");
     IfNo:
     Console.Write("G");
-    goto punkttest;
+    
 
 }
-exit:
+exit:*/
 #if DEBUG
     Console.WriteLine("Press enter to close...");
     Console.ReadLine();

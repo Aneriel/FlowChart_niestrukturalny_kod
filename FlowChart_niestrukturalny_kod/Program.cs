60641428c4a388701d02cc3d744d25035f518c42
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-11-07 15:25:45 +0100

    #00-czyszczenie_brancha

--- FlowChart_niestrukturalny_kod/Program.cs
+++ FlowChart_niestrukturalny_kod/Program.cs
@@ -12,40 +12,7 @@ namespace FlowChart
             int x = data[0];
             int y = data[1];
             int z = data[2];
-        begining:
-            if (x > 0)
-            {
-                if (y > 0)
-                {
-                    x = x - 1;
-                    y = y - 1;
-                    Console.Write("C");
-                    goto begining;
-                }
-                else
-                {
-                    Console.Write("D");
-
-                }
-
-            }
-            else
-            {
-                Console.Write("E");
-                goto g;
-            }
-            if (z > 0)
-            {
-                goto end;
-            }
-
-        g:
-            Console.Write("G");
-
-
-        end:
-            Console.WriteLine();
-
+        
         }
     }
 }
